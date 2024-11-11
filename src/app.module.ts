194ec9497991f2093c2d121a5789cbf82ac035b6
lywq9296 <ywli0314@sina.com>
2024-11-11 21:44:40 +0800

    feat: typeorm 连接 mysql 数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { BookModule } from './modules/book/book.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [UserModule, AuthModule, BookModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      type: 'mysql',
+      database: process.env.DATABASE,
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
