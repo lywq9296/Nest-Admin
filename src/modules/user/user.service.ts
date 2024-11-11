f68953151c0fcd8887b717aa60c14cf0793b09a4
lywq9296 <ywli0314@sina.com>
2024-11-12 06:04:29 +0800

    feat: 数据库实现增删改查

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,5 +13,24 @@ export class UserService {
 
   findOne(id: number): Promise<User> {
     return this.userRepository.findOneBy({ id });
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+    user.role = createUserDto.role;
+    user.avatar = createUserDto.avatar;
+    user.active = 1;
+
+    return this.userRepository.save(user);
+  }
+
+  remove(id: number): Promise<DeleteResult> {
+    return this.userRepository.delete(id);
   }
 }
