da2c22309f31471bbab7b1f56aac2b13d18a56f9
lywq9296 <ywli0314@sina.com>
2024-11-13 05:55:33 +0800

    feat: 登录接口login -- 密码校验逻辑

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from 'src/decorators/public.decorator';
+import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
   @Public()
+  @Post('login')
+  @UseFilters(new HttpExceptionFilter())
   login(@Body() params) {
-    return this.authService.login(params.username);
+    return this.authService.login(params.username, params.password);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,19 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
-
+import * as md5 from 'md5';
 @Injectable()
 export class AuthService {
   @Inject()
   private userService: UserService;
 
-  async login(username) {
+  async login(username, password) {
     const user = await this.userService.findByUsername(username);
+    const md5Password = md5(password).toUpperCase();
+
+    if (user.password !== md5Password) {
+      throw new UnauthorizedException();
+    }
+
     return user;
   }
 }
