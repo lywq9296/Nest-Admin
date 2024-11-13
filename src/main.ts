441d8219f39cbacd758893f2781d87fbeb1df4d1
lywq9296 <ywli0314@sina.com>
2024-11-14 07:18:11 +0800

    feat: nest 服务支持 https 协议

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const httpsOptions = {
+  key: fs.readFileSync(path.resolve(process.cwd(), '../https/nest-admin.key')),
+  cert: fs.readFileSync(path.resolve(process.cwd(), '../https/nest-admin.pem')),
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const httpsApp = await NestFactory.create(AppModule, {
+    cors: true,
+    httpsOptions,
+  });
+  await httpsApp.listen(8000);
+
+  const httpApp = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  await httpApp.listen(8001);
 }
 
 bootstrap();
