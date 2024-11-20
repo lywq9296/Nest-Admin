5e182f00921cb150e191836d5067c005614e9036
lywq9296 <ywli0314@sina.com>
2024-11-21 06:36:08 +0800

    perf: 文件上传 API 功能优化

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -3,13 +3,14 @@ import * as path from 'path';
 import {
   Controller,
   Get,
+  ParseFilePipeBuilder,
   Post,
   Query,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { BookService } from './book.service';
-import { wrapperCountResponse } from 'src/utils/response';
+import { wrapperCountResponse, wrapperResponse } from 'src/utils/response';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('book')
@@ -27,11 +28,29 @@ export class BookController {
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file')) // 接收参数为 file
-  updateBook(@UploadedFile() file: Express.Multer.File) {
+  updateBook(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({ fileType: /epub/ }) // 限制上传文件格式
+        .build(),
+    )
+    file: Express.Multer.File,
+  ) {
     console.log(file);
 
-    const desPath = path.resolve(process.cwd(), 'var/www/upload'); // 接收路径, 存储文件的地址
+    const desDir = path.resolve(process.cwd(), process.env.UPLOAD_FILE_PATH); // 接收路径, 存储文件的地址
+    const desPath = path.resolve(desDir, file.originalname);
+    fs.writeFileSync(desPath, file.buffer);
 
-    fs.writeFileSync(path.resolve(desPath, file.originalname), file.buffer);
+    return wrapperResponse(
+      Promise.resolve().then(() => ({
+        originalname: file.originalname,
+        mimetype: file.mimetype,
+        size: file.size,
+        path: desPath,
+        dir: desDir,
+      })),
+      '文件上传成功',
+    );
   }
 }
