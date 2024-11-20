3e6996e45a5af967614b6ddd7b5e709eb3f3378b
lywq9296 <ywli0314@sina.com>
2024-11-21 06:21:43 +0800

    feat: 文件上传API基本功能

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { wrapperCountResponse } from 'src/utils/response';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('book')
 export class BookController {
@@ -13,5 +23,15 @@ export class BookController {
       this.bookService.countBookList(params),
       '获取图书列表成功',
     );
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file')) // 接收参数为 file
+  updateBook(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+
+    const desPath = path.resolve(process.cwd(), 'var/www/upload'); // 接收路径, 存储文件的地址
+
+    fs.writeFileSync(path.resolve(desPath, file.originalname), file.buffer);
   }
 }
